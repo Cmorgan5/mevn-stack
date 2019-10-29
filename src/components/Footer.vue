69bc9ed21e129c92269e8e00fd7b14ad77337dab
Cory Morgan <cory.morgan@purered.net>
2019-10-28 23:55:58 -0400

    Setting up base before add auth and taks

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer id="custom-footer">
+        <span class="mr-4"> &copy; 2019 Cory Morgan</span>
+        <span class="mr-4"> Privacy Policy</span>
+        <span>Terms</span>
+    </footer>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="#">
+        <img style="max-height:25px;" alt="Vue logo" src="../assets/logo.png">Task Manager</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Tasks</a>
+            </li>
+            
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Logout</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cory</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </header>
+</template>
