f2e63fdf895a641c2ffa01e28c60319768ae7b9d
Cory Morgan <cory.morgan@purered.net>
2019-10-30 10:12:17 -0400

    Env + CORS config

--- dev-server/index.js
+++ dev-server/index.js
@@ -3,11 +3,21 @@
 import express from 'express';
 const app = express()
 import { registerRoutes } from './routes';
+import { setEnvironment } from './config/env';
 
+setEnvironment(app);
 registerRoutes(app);
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+    if(process.env.NODE_ENV !== 'production') {
+        return res.send(
+            'Running server in development mode.'
+        );
+    } else {
+        return res.sendFile('index.html', { root: __dirname + '/../dist'});
+    }
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!` + process.env.NODE_ENV + 'mode!'))
--- prod-server/index.js
+++ prod-server/index.js
@@ -6,8 +6,11 @@ var _express = _interopRequireDefault(require("express"));
 
 var _routes = require("./routes");
 
+var _env = require("./config/env");
+
 // const express = require('express')
 var app = (0, _express.default)();
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 var port = 3000;
 app.get('/', function (req, res) {
